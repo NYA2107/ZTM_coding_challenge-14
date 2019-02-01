275efaec0ea61da77bbd1329812d698a1a17d39a
zesagata <zezagata17@gmail.com>
2019-02-01 16:12:40 +0700

    Edit function in game engine

--- js/game-engine.js
+++ js/game-engine.js
@@ -4,6 +4,8 @@ class GE{
         this.GRID = this.create2DGrid(imageTotal);
         this.DONE_COUNTER = imageTotal;
         this.CURRENT = null;
+        this.CURR_X = -1;
+        this.CURR_Y = -1;
     }
     /**
      * Will Create 2D Array
@@ -39,16 +41,29 @@ class GE{
     /**
      * Will check
      * if this.GRID[m][n] == this.CURRENT && this.CURRENT != null
-     *     mark this.GRID[m][n] as null
+     *     mark this.GRID[m][n] as x
+     *     reset this.CURR_X to be -1
+     *     reset this.CURR_Y to be -1
      *     reset this.CURRENT to be null
      *     this.DONE_COUNTER -= 1;
      * else if this.GRID[m][n] != this.CURRENT && this.CURRENT !+ null
      *     reset this.CURRENT to be null
      * else if this.CURRENT = null
      *      set this.CURRENT = this.GRID[m][n]
+     *      set this.CURR_X = m
+     *      set this.CURR_Y = n
      */
     checkGrid(m,n){
-
+        if(this.GRID[m][n] == this.CURRENT && this.CURRENT != null){
+            this.CURRENT = null;
+            this.GRID[m][n] = "x";
+        }else if(this.GRID[m][n] != this.CURRENT && this.CURRENT != null){
+            this.CURRENT = null;
+        }else if(this.CURRENT == null){
+            this.CURRENT = this.GRID[m][n];
+            this.CURR_X = m;
+            this.CURR_Y = n;
+        }
         isItDone();
     }
     /**
@@ -59,6 +74,10 @@ class GE{
      *      return false
      */
     isItDone(){
-
+        if(this.DONE_COUNTER == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
